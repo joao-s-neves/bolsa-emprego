e1fd2106aef11ab4338294d037be379e425af1f6
joao-s-neves <joneves14@gmail.com>
2017-01-06 12:11:28 +0000

    Introduzir Homepage

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Bolsa de Emprego"
+  end
+
+  test "should get root" do
+    get '/'
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Bolsa de Emprego"
   end
 
 end
